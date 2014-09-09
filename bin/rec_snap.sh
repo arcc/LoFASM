b41ae5ecda4f73db43e8744619466fae107b9515
Louis P. Dartez <louis.dartez@gmail.com>
2014-09-09 13:55:37 -0500

    added setup_env.sh to read STATION envvar and setup corresponding env modified rec_snap.sh to copy latest data file to specified folder 	using env vars created by setup_env.sh

--- bin/rec_snap.sh
+++ bin/rec_snap.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-/home/controller/LoCo_Control/ten_gbe_recorder.py -t 15
+# acquire data
+ten_gbe_recorder.py -t 15
+
+data_home=$LOFASMDATA_HOME
+data_recent=$LOFASMDATA_RECENT
+
+#get latest directory
+latest_directory=$( ls -Art $data_home | tail -n 1)
+
+#get latest filename
+latest_filename=$( ls -Art $data_home/* | tail -n 1)
+
+#clear last copy
+rm $data_recent/*
+
+#copy new file
+cp $latest_directory/$latest_filename $data_recent/
---
+++ bin/setup_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#this script sets the environment variables for each LoFASM Station.
+#this script feeds off of the already set STATION environment variable.
+#if $STATION is not set then this script will fail.
+
+station=$STATION 
+
+case "$station" in
+LoFASMI) echo "Setting up LoFASMI environment..."
+	;;
+LoFASMII) echo "Setting up LoFASMII environment..."
+	;;
+LoFASMIII) echo "Setting up LoFASMIII environment..."
+	;;
+LoFASMIV) echo "Setting up LoFASMIV environment..."
+	;;
+*) echo "Not a valid LoFASM station!"
+	exit
+	;;
+esac
+
