584d2274c390c839030673704c74045560afce57
Louis P. Dartez <louis.dartez@gmail.com>
2014-09-14 21:04:03 -0500

    added init_roach script to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Louis P. Dartez',
     author_email='louis.dartez@gmail.com',
     packages=['lofasm'],
-    scripts=['bin/initialize.py','bin/ten_gbe_recorder.py','bin/poco_plot.py', 'bin/rec_snap.sh'],
+    scripts=['bin/initialize.py','bin/ten_gbe_recorder.py','bin/poco_plot.py', 'bin/rec_snap.sh', 'bin/init_roach.py'],
     description='LoFASM Tools',
     long_description=open('README').read(),
     install_requires=[
