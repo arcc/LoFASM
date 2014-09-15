eb64d112ab057c737909516837697d3cee95c34e
Louis P. Dartez <louis.dartez@gmail.com>
2014-09-14 20:32:39 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Louis P. Dartez',
     author_email='louis.dartez@gmail.com',
     packages=['lofasm'],
-    scripts=['bin/initialize.py','bin/ten_gbe_recorder.py','bin/poco_plot.py', 'bin/rec_snap.sh', 'bin/setup_lofasm.sh'],
+    scripts=['bin/initialize.py','bin/ten_gbe_recorder.py','bin/poco_plot.py', 'bin/rec_snap.sh'],
     description='LoFASM Tools',
     long_description=open('README').read(),
     install_requires=[
