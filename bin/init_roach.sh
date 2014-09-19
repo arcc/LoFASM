5a673267cfbee495ad84d39ee5ce20eea488a527
Louis P. Dartez <louis.dartez@gmail.com>
2014-09-18 22:47:42 -0500

    init_roach will use roach ip from env var.

--- bin/init_roach.sh
+++ bin/init_roach.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-/home/controller/LoCo_Control/initialize.py 192.168.4.21 -g1 -l8192 -bpoco_14b_no10gsnap_2014_Jun_03_1705.bof
+/usr/local/bin/initialize.py $ROACH_IP -g1 -l8192 -bpoco_14b_no10gsnap_2014_Jun_03_1705.bof
