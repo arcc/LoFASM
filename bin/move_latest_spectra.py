b77e8ddd29a4e5d0412968325b85d9961c1462b5
Louis Dartez <louis.dartez@gmail.com>
2014-09-19 19:23:14 -0600

    modifications to move_spectra py file

--- bin/move_latest_spectra.py
+++ bin/move_latest_spectra.py
@@ -13,7 +13,7 @@ buffer_time = datetime.timedelta(hours = 24)
 now = datetime.datetime.utcnow()
 
 #get latest lofasm files
-latest_files = future.get_spectra_range(now - buffer_time, now, data_home)
+files_to_copy = future.get_spectra_range(now - buffer_time, now, data_home)
 
 #get list of files in recent directory
 listdir = os.listdir(data_recent)
@@ -25,26 +25,36 @@ recent_files = []
 for item in listdir:
     if item[-7:] == '.lofasm':
         recent_files.append(future.LoFASM_file(data_recent+'/'+item))
+    else:
+        #prune non-lofasm files
+        print "pruning %s " % item
+        rm_cmd = "rm -f %s" %( data_recent + '/' + item)
+
 
 
 for f in recent_files:
-    if f in latest_files:
-        latest_files.remove(f)
+    if f in files_to_copy:
+        #already copied
+        files_to_copy.remove(f)
     else:
         #remove file from recent directory
         rm_cmd = 'rm -f %s' % (f.parent+'/'+f.name)
-        #print rm_cmd
+        print rm_cmd
         syscmd(rm_cmd)
 
         
-for latest_file in latest_files:
+for latest_file in files_to_copy:
+    if not isinstance(latest_file,future.LoFASM_file):
+        print "ignoring", latest_file
+        pass
     if future.get_total_file_size(latest_file) < 100:
+        print "file too small"
         pass
     else:
         #file needs to placed in the recent directory
         print "copying %s" % latest_file.parent+'/'+latest_file.name
         cp_cmd = 'cp %s %s' % (latest_file.parent + '/' + latest_file.name, data_recent+'/.')
-        #print cp_cmd
+        print cp_cmd
         syscmd(cp_cmd)
 
 
