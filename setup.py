18871cf74a67f2b4c71f4c67234f61511b09f2e6
Louis P. Dartez <louis.dartez@gmail.com>
2015-07-30 04:16:42 -0500

    updated setup script

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
 
     scripts=['bin/initialize.py','bin/ten_gbe_recorder.py','bin/lofasm_plot.py',
     'bin/rec_snap.sh', 'bin/init_roach.sh', 'bin/simulate_signal_as_AA.py',
-    'bin/simulate_zeros_as_AA.py', 'bin/lofasm-chop.py','bin/LoFASM_GUI.py'],
+    'bin/simulate_zeros_as_AA.py', 'bin/lofasm-chop.py','bin/LoFASM_GUI.py',
+    'bin/sched_parser.py'],
     description='LoFASM Tools',
     long_description=open('README.md').read(),
     install_requires=[
