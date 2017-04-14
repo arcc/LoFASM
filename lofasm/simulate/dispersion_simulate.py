b8777e7136d0e25fb6caa786aa60df2c76df1ff0
luojing1211 <luojing1211@gmail.com>
2017-04-14 16:18:40 -0500

    Add a better time resolution

--- lofasm/simulate/dispersion_simulate.py
+++ lofasm/simulate/dispersion_simulate.py
@@ -24,9 +24,21 @@ def disperse_filterbank(dm, filter_bank_data, full_result=False):
     extra_time_delay = dm_delay(dm, extra_low, freqs[-1])
     last_time_delay = dm_delay(dm, freqs[0], freqs[-1])
     last_time_span = extra_time_delay - last_time_delay
-    max_smear = int(last_time_span / filter_bank_data.time_resolution) + 2
+    max_smear = int(last_time_span / filter_bank_data.time_resolution) + 3
+
     # Get a finer resolution of frequency
     fine_freqs = np.zeros((len(filter_bank_data.freq_axis), max_smear))
+    fine_times = np.zeros((len(filter_bank_data.freq_axis), max_smear))
+    # First We need a finer time resolution
+    delay_prim = dm_delay(dm, freqs, freqs[-1])
+    delay_prim_index = delay_prim/filter_bank_data.time_resolution
+    delay_prim_index = delay_prim_index.astype(int)
+    index_time = delay_prim_index * filter_bank_data.time_resolution
+    freq_enter_time = delay_prim - index_time
+    # construct a finer time resolution
+    fine_times.fill(filter_bank_data.time_resolution)
+    fine_times[:, 0] = filter_bank_data.time_resolution - freq_enter_time
+    # get_finer_freq
     back_freqs = np.append(freqs[::-1], extra_low)
     end_freqs = np.delete(back_freqs, 0)
     for ii, f in enumerate(back_freqs[0:-1]):
@@ -37,18 +49,17 @@ def disperse_filterbank(dm, filter_bank_data, full_result=False):
         fine_freqs[fine_freqs.shape[0] - ii - 1 ][itr] = f
         while True:
             itr += 1
-            f_low = get_freq_from_time(f_low, filter_bank_data.time_resolution, dm)
+            res_time = fine_times[fine_freqs.shape[0] - ii - 1][itr-1]
+            f_low = get_freq_from_time(f_low, res_time, dm)
             if f_low < back_freqs[ii + 1]:
                 break
             fine_freqs[fine_freqs.shape[0] - ii -1][itr] = f_low
-
     # Get fine_freqs powers.
     power_distr = np.zeros((len(freqs), max_smear - 1))
     for ii in range(max_smear - 1):
         power_distr[:, ii] = (fine_freqs[:,ii] - \
                   fine_freqs[:,ii+1])/freq_res
-    delay_prim = dm_delay(dm, freqs, freqs[-1])/filter_bank_data.time_resolution
-    delay_prim = delay_prim.astype(int)
+
     if not full_result:
         dispersed = fbs.FilterBank(filter_bank_data.name + '_dispersed',
                                num_time_bin=filter_bank_data.num_time_bin, \
@@ -72,7 +83,7 @@ def disperse_filterbank(dm, filter_bank_data, full_result=False):
     for ii in range(filter_bank_data.num_time_bin):
         powers = power_distr * filter_bank_data.data[ii, :][:, np.newaxis]
         for jj in range(filter_bank_data.num_freq_bin):
-            target_time_idx = ii + delay_prim[jj]
+            target_time_idx = ii + delay_prim_index[jj]
             changed_idx = np.arange(target_time_idx, target_time_idx+powers.shape[1])
             d_idx = dispersed.num_time_bin - changed_idx
             effect = changed_idx[np.where(d_idx>0)[0]]
