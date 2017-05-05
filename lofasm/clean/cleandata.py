c88827cdc86b79b3574461433f7d94853683d7c7
luojing1211 <luojing1211@gmail.com>
2017-05-05 01:03:38 -0500

    Fix bugs, optimize input

--- lofasm/clean/cleandata.py
+++ lofasm/clean/cleandata.py
@@ -229,7 +229,7 @@ def wide_band_mask(data,threshold=1.7):
 	return wb_mask, percent_clean_time
 
 if __name__ == '__main__':
-	'''
+    '''
 	try:
     	os.makedirs('Cleaned/')
 	except OSError:
@@ -238,86 +238,95 @@ if __name__ == '__main__':
     '''
 
 
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('-lf_bin', action='store', dest='lower_frequency_bin',
-	default = 200, help='What is the lower frequency bin?')
+    parser.add_argument('-lf_bin', action='store', dest='lower_frequency_bin',
+    default = 0, help='What is the lower frequency bin?')
 
-	parser.add_argument('-uf_bin', action='store', dest='upper_frequnecy_bin',
-	default = 700, help='What is the upper frequency bin?')
+    parser.add_argument('-uf_bin', action='store', dest='upper_frequency_bin',
+    default = 0, help='What is the upper frequency bin?')
 
-	parser.add_argument('-f', dest='fast',
-	default = False, help='Do you want to normalize with the fast option?')
+    parser.add_argument('-f', dest='fast',
+    default = False, help='Do you want to normalize with the fast option?')
 
-	parser.add_argument('-ot', action='store', dest='o_thresh',
-	default = 10, help='What is the threshold to be used for creating the outlier mask?')
+    parser.add_argument('-ot', action='store', dest='o_thresh',
+    default = 10, help='What is the threshold to be used for creating the outlier mask?')
 
-	parser.add_argument('-nt', action='store', dest='nb_thresh',
-	default = 1, help='What is the threshold to be used for creating the narrow band mask?')
+    parser.add_argument('-nt', action='store', dest='nb_thresh',
+    default = 1, help='What is the threshold to be used for creating the narrow band mask?')
 
-	parser.add_argument('-wt', action='store', dest='wb_thresh',
-	default = 1.7, help='What is the threshold to be used for creating the wide band mask?')
+    parser.add_argument('-wt', action='store', dest='wb_thresh',
+    default = 1.7, help='What is the threshold to be used for creating the wide band mask?')
 
-	parser.add_argument('-pol', action='store', dest='polarization',
-	default = 'CC', help = 'what is the polarization you want to clean?')
+    parser.add_argument('-pol', action='store', dest='polarization',
+    default = 'CC', help = 'what is the polarization you want to clean?')
 
-	parser.add_argument('-fn', action='store', dest='file_name',
-	default = '*.bbx.gz',
-	help = 'do you want to clean a specific file or type of file? try ')
+    parser.add_argument('-fn', action='store', dest='file_name',
+    default = '*.bbx.gz',
+    help = 'do you want to clean a specific file or type of file? try ')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	lower_frequency = args.lower_frequency_bin*(100/1024)
-	upper_frequency = args.upper_frequnecy_bin*(100/1024)
+	# lower_frequency = args.lower_frequency_bin*(100/1024)
+	# upper_frequency = args.upper_frequnecy_bin*(100/1024)
 
-	for fname in glob.glob(args.file_name):
+    for fname in glob.glob(args.file_name):
+        lf = b.LofasmFile(fname)
+        lf.read_data()
+        if args.upper_frequency_bin == 0:
+            args.upper_frequency_bin = int(lf.header['metadata']['dim2_len'])
+        if 'frequency_offset_DC' in lf.header.keys():
+            freq_off_set_DC = float(lf.header['frequency_offset_DC'].split()[0])
+        else:
+            freq_off_set_DC = 0.0
 
-
-		lf = b.LofasmFile(fname)
-		lf.read_data()
-		data = 10*np.log10(lf.data[args.lower_frequency_bin:args.upper_frequnecy_bin])
-
-
-		norm_data, normalize_array 									= normalize(data,fast=args.fast)
-		o_mask   , outlier_average_top, outlier_average_bottom		= outlier_mask(norm_data,threshold= args.o_thresh)
-		nb_mask  , percent_clean_freq_channels 						= narrow_band_mask(norm_data,threshold= args.nb_thresh)
-		wb_mask  , percent_clean_time 								= wide_band_mask(norm_data,threshold= args.wb_thresh)
-
-		print fname + '---------------------------'
-		print 'top outlier average               : '+str(outlier_average_top)
-		print 'bottom outlier average            : '+str(outlier_average_bottom)
-		print 'percentage of clean freq channels : '+str(percent_clean_freq_channels)
-		print 'percentage of clean time          : '+str(percent_clean_time)
-		print ''
-		print '-------------------------------------------------------'
-
-		final = norm_data*wb_mask*o_mask*nb_mask
-		final[np.isnan(final)]=1
-		final = final*normalize_array
+        freq_step = float(lf.header['dim2_span'])/float(lf.header['metadata']['dim2_len'])
+        freq_start = float(lf.header['dim2_start']) + freq_off_set_DC
+        lower_frequency = freq_start + args.lower_frequency_bin * freq_step
+        upper_frequency = freq_start + args.upper_frequency_bin * freq_step
+        print lower_frequency, upper_frequency
+        data = 10*np.log10(lf.data[args.lower_frequency_bin:args.upper_frequency_bin])
 
 
-		lfc = b.LofasmFile('Clean_' + fname.rstrip('.gz'), mode = 'write')
+        norm_data, normalize_array 									= normalize(data,fast=args.fast)
+        o_mask   , outlier_average_top, outlier_average_bottom		= outlier_mask(norm_data,threshold= args.o_thresh)
+        nb_mask  , percent_clean_freq_channels 						= narrow_band_mask(norm_data,threshold= args.nb_thresh)
+        wb_mask  , percent_clean_time 								= wide_band_mask(norm_data,threshold= args.wb_thresh)
 
-		lfc.add_data(final)
+        print fname + '---------------------------'
+        print 'top outlier average               : '+str(outlier_average_top)
+        print 'bottom outlier average            : '+str(outlier_average_bottom)
+        print 'percentage of clean freq channels : '+str(percent_clean_freq_channels)
+        print 'percentage of clean time          : '+str(percent_clean_time)
+        print ''
+        print '-------------------------------------------------------'
+
+        final = norm_data*wb_mask*o_mask*nb_mask
+        final[np.isnan(final)]=1
+        final = final*normalize_array
+
+
+        lfc = b.LofasmFile('Clean_' + fname.rstrip('.gz'), mode = 'write')
+
+        lfc.add_data(final)
 
 
 
-		lfc.set('dim1_start',lf.header['dim1_start'])
-		lfc.set('dim1_span',lf.header['dim1_span'])
-		lfc.set('dim2_start',lower_frequency * 1e6)
-		lfc.set('dim2_span',(upper_frequency - lower_frequency)*1e6)
-		lfc.set('clean', True)
-		lfc.set('station',lf.header['station'])
-		lfc.set('channel',lf.header['channel'])
+        lfc.set('dim1_start',lf.header['dim1_start'])
+        lfc.set('dim1_span',lf.header['dim1_span'])
+        lfc.set('dim2_start',lower_frequency )
+        lfc.set('dim2_span',(upper_frequency - lower_frequency))
+        lfc.set('clean', True)
+        lfc.set('station',lf.header['station'])
+        lfc.set('channel',lf.header['channel'])
 
-		lfc.write()
-		lfc.close()
+        lfc.write()
+        lfc.close()
 
-		print 'done'
+        print 'done'
 
-		'''
-		plt.imshow(final, aspect = 'auto', origin = 'lower', cmap='gray')
-		plt.colorbar()
-		plt.savefig('Cleaned/' + fname.rstrip('.bbx.gz') + '.png', dpi=200)
-		plt.close()
-		'''
+
+		# plt.imshow(final, aspect = 'auto', origin = 'lower', cmap='gray')
+		# plt.colorbar()
+		# plt.savefig('Cleaned/' + fname.rstrip('.bbx.gz') + '.png', dpi=200)
+		# plt.close()
