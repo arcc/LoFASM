f8c53fccc67b9fddfef231137a5e634b5398d9fb
Louis Dartez <louis.dartez@gmail.com>
2017-05-31 20:10:23 -0500

    added support for gzopen.

--- lofasm/parse_data.py
+++ lofasm/parse_data.py
@@ -114,6 +114,7 @@ def parse_file_header(file_obj, fileType='lofasm'):
 
     if freeze_pointer != 0:
         file_obj.seek(0)
+
     #get file signature
     file_sig = file_obj.read(pdat_H.HDR_ENTRY_LENGTH).strip(' ')
 
@@ -184,7 +185,7 @@ def parse_file_header(file_obj, fileType='lofasm'):
     file_obj.seek(freeze_pointer)
     return fhdr_field_dict
 
-def parse_hdr(hdr, hdr_size_bytes=8, version=1):
+def parse_hdr(hdr):
     '''
     Usage: parse_hdr(<64bit_string>,[version])
     Parse the first 64 bits of
@@ -203,6 +204,10 @@ def parse_hdr(hdr, hdr_size_bytes=8, version=1):
     return hdr_dict
 
 def print_hdr(hdr_dict):
+    '''
+    print header dictionary contents
+    '''
+
     for key in hdr_dict:
         val = str(hdr_dict[key])
         if val != '0':
@@ -300,11 +305,15 @@ def get_filesize(file_obj):
     '''
 
     freeze_pointer = file_obj.tell()
-    file_obj.seek(0,2) #move to end of file
-    end_pointer = file_obj.tell()
-    file_obj.seek(freeze_pointer)
 
-    return end_pointer
+    if type(file_obj) == gzip.GzipFile:
+        hdr = parse_file_header(file_obj)
+        return int(hdr[12][1])*INTEGRATION_SIZE_B + 96
+    else:
+        file_obj.seek(0,2) #move to end of file
+        end_pointer = file_obj.tell()
+        file_obj.seek(freeze_pointer)
+        return end_pointer
 
 def get_number_of_integrations(file_obj):
     '''returns number of integrations in data file'''
@@ -625,14 +634,17 @@ class LoFASMFileCrawler(object):
         integration headers in file. This is an optional argrument.
         '''
 
+
         self.filename = filename
         self.scan_file = scan_file
         self.start_loc = start_loc
         self._status_open = False
         self.corrupt = False
-        self.gz = gz
+        if filename.endswith('.gz') or gz:
+            self.gz = True
+        else:
+            self.gz = gz
         self.eof = False
-
         self._int_hdr = {} #integration header
         self._file_hdr = {} #file header
         self._acc_num_ref = None #first integration id
@@ -668,6 +680,7 @@ class LoFASMFileCrawler(object):
 
                 #get file handler
                 if self.gz:
+                    print "Warning: gzipped files are only supported for header versions 4 or higher."
                     self._lofasm_file = gzip.open(filename, 'rb')
                 else:
                     self._lofasm_file = open(filename, 'rb')
@@ -681,7 +694,10 @@ class LoFASMFileCrawler(object):
         self._file_hdr = parse_file_header(self._lofasm_file)
 
         #find end of file
-        #self._lofasm_file_end = self._get_file_end_loc()
+        if int(self._file_hdr[2][1]) == 4:
+            self._lofasm_file_end = self._file_hdr[3][1] + int(self._file_hdr[12][1])*INTEGRATION_SIZE_B
+        else:
+            self._lofasm_file_end = self._get_file_end_loc()
 
         #get integration/burst size
         self._int_size = INTEGRATION_SIZE_B
@@ -764,7 +780,7 @@ class LoFASMFileCrawler(object):
         '''
 
         # check file header version
-        if self._file_hdr[2][1] == 4:
+        if int(self._file_hdr[2][1]) == 4:
             return int(self._file_hdr[12][1])
         else:
             return int((self._lofasm_file_end - self._data_start) / self._int_size)
